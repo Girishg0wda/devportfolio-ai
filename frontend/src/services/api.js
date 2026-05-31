4361c41adfbb852bdee758779a49f27199249638
Girish <girishgowdasr428@gmail.com>
2026-05-31 17:01:17 +0530

    Fix JWT authorization headers

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -6,4 +6,21 @@ const api = axios.create({
     "http://127.0.0.1:8000",
 });
 
+api.interceptors.request.use(
+  (config) => {
+    const token =
+      localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization =
+        `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default api;
